eb4f8c3c7e686bda113479b87a5e3bcf8f6e3bf2
Aguiar <joaomatheusaguiar@gmail.com>
2017-12-18 17:49:41 -0300

    adicionando express-validator

--- app/routes/criar_noticia.js
+++ app/routes/criar_noticia.js
@@ -5,9 +5,23 @@ module.exports = function(app){
 
     app.post('/noticias/salvar',function(req, res){
         var noticia = req.body;
+        
+        req.assert('titulo','Título é obrigatório').notEmpty();
+        req.assert('resumo','Resumo é obrigatório').notEmpty();
+        req.assert('resumo','Resumo deve conter entre 10 a 100 caracteres').len(10, 100);
+        req.assert('autor','Autor é obrigatório').notEmpty();
+        req.assert('data_noticia','Data é obrigatório').notEmpty();
+        req.assert('noticia','Notícia é obrigatório').notEmpty();
+
+        var erros = req.validationErrors();
+
+        if(erros){
+            res.render('admin/form_add_noticia');
+            return;
+        }
+
         var connection = app.config.dbConnection(); // importado pelo consign
         var noticiasModel = new app.app.models.NoticiasDAO(connection); // importado pelo consign
-
         noticiasModel.salvarNoticia(noticia, function(erro, result){
           res.redirect('/noticias');
         });
--- config/server.js
+++ config/server.js
@@ -1,16 +1,19 @@
 var express = require('express'); // retorna uma função
 var app = express(); // executa a função do express
 var bodyParser = require('body-parser');
-var consign = require('consign'); // 
+var consign = require('consign');
+var expressValidator = require('express-validator');
 
 app.set('view engine', 'ejs'); // modificar a engine de view do express
 app.set('views', './app/views'); // mudar o caminho da pasta de views
 
 app.use(bodyParser.urlencoded({extended: true})); // adicionar o body parser no express (middleware)
 
+app.use(expressValidator()); // adicionando no express como middleware
+
 consign()
-    .include('./app/routes')
-    .then('app/models')
+    .include('./app/routes') // faz o require das rotas dentro do app !
+    .then('app/models') // depois de incluir adiciona os models
     .then('config/dbConnection.js') // fazer o require da conexao com o banco de dados depois de importar as rotas / Ele já executa o módulo
     .into(app); // scanneia o diretório e faz os imports dos módulos dentro do app 
 
